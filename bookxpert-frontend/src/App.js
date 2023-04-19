f552663aa8c0fafb295c99f1cb71b928987b51eb
Durgesh9871 <devisingh9871686098@gmail.com>
2023-04-19 13:33:58 +0530

    file

--- bookxpert-frontend/src/App.js
+++ bookxpert-frontend/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-  Divider,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
-
 function App() {
 
   
   
   return (
     <div>
-
+  hello world
     </div>
   );
 }
